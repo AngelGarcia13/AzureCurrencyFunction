d1a9679d45178b0844f3c19221853b3f874b8d76
Angel Garcia <angelrenegarcia13@gmail.com>
2020-06-28 04:33:32 +0200

    Function Code Done!

---
+++ src/CurrencyFunction.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using AngleSharp;
+using AngleSharp.Html.Parser;
+using System.Linq;
+using AngleSharp.Html.Dom;
+
+namespace Currency
+{
+    public static class CurrencyFunction
+    {
+        [FunctionName("CurrencyFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var config = Configuration.Default.WithDefaultLoader();
+            var address = "https://www.banreservas.com/calculators/divisas";
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(address);
+            
+            var dollarBuySelector = "#edit-buy-wrapper .form-label-text";
+            var valueDollarBuy = document.QuerySelectorAll(dollarBuySelector);
+            var value = (valueDollarBuy.FirstOrDefault().TextContent).Split('=')[1];
+            value = value.Substring(value.LastIndexOf("RD$") + 3).Trim();
+            return new OkObjectResult(new {
+                Currency = "USD",
+                Rate = value,
+                RateCurrency = "DOP"
+            });
+        }
+    }
+}
